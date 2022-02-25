371b70af6aa930ccf9a515326409c57352614ae2
Robert Coffey <foggynight709@gmail.com>
2022-02-25 16:56:39 -0330

    fix: bounds of strncpy and terminate word

--- rpn-calc.c
+++ rpn-calc.c
@@ -7,7 +7,7 @@
 
 #define LINE_SIZE  (1 << 8)
 #define STACK_SIZE (1 << 8)
-#define WORD_SIZE  25
+#define WORD_SIZE  512
 
 #define min(X, Y) ((X > Y) ? Y : X)
 #define max(X, Y) ((X < Y) ? Y : X)
@@ -20,19 +20,21 @@ double stack_pop(void) { return stack[--stack_ptr]; }
 void stack_clear(void) { stack_ptr = 0; }
 
 int inter_word(const char *line, size_t word_start, size_t word_end) {
-    static char str[WORD_SIZE];
-    strncpy(str, line + word_start, min(word_end, 21));
-    if (isdigit(str[0])
-        || ((str[0] == '-' || str[0] == '+')
+    static char word[WORD_SIZE];
+    const size_t word_len = word_end - word_start;
+    strncpy(word, line + word_start, min(word_len, WORD_SIZE-1));
+    word[word_len] = '\0';
+    if (isdigit(word[0])
+        || ((word[0] == '-' || word[0] == '+')
             && (word_end > word_start + 1)
-            && isdigit(str[1]))) {
+            && isdigit(word[1]))) {
         char *end;
-        double n = strtod(str, &end);
+        double n = strtod(word, &end);
         stack_push(n);
     }
     else if (word_start == word_end - 1) {
         double e;
-        switch (str[0]) {
+        switch (word[0]) {
         case '+': e = stack_pop(); stack[stack_ptr-1] += e; break;
         case '-': e = stack_pop(); stack[stack_ptr-1] -= e; break;
         case '*': e = stack_pop(); stack[stack_ptr-1] *= e; break;
