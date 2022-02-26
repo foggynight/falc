182bfa79bb0141fd0dbc4786b724026c76f52db5
Robert Coffey <foggynight709@gmail.com>
2022-02-25 21:06:29 -0330

    feat: add dictionary and store operation

--- rpn-calc.c
+++ rpn-calc.c
@@ -5,24 +5,33 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define LINE_LIM  (1 << 8)
-#define STACK_LIM (1 << 8)
-#define WORD_LIM  512
+#define DICT_SIZE  ('z' - 'a' + 1)
+#define LINE_SIZE  (1 << 8)
+#define STACK_SIZE (1 << 8)
+#define WORD_SIZE  (1 << 9)
 
 #define min(X, Y) ((X > Y) ? Y : X)
 #define max(X, Y) ((X < Y) ? Y : X)
 
-double stack[STACK_LIM];
+double stack[STACK_SIZE];
 size_t stack_ptr = 0;
 
-void stack_push(double n) { if (stack_ptr < STACK_LIM) stack[stack_ptr++] = n; }
+void stack_push(double n) { if (stack_ptr < STACK_SIZE) stack[stack_ptr++] = n;}
 double stack_pop(void) { return !stack_ptr ? 0 : stack[--stack_ptr]; }
 void stack_clear(void) { stack_ptr = 0; }
 
+enum dict_state { FETCH, STORE };
+enum dict_state state = FETCH;
+
+double dict[DICT_SIZE];
+
+double dict_get(char key) { return dict[key % DICT_SIZE]; }
+void dict_set(char key, double n) { dict[key % DICT_SIZE] = n; }
+
 int inter_word(const char *line, size_t word_start, size_t word_end) {
-    static char word[WORD_LIM];
+    static char word[WORD_SIZE];
     const size_t word_len = word_end - word_start;
-    strncpy(word, line + word_start, min(word_len, WORD_LIM-1));
+    strncpy(word, line + word_start, min(word_len, WORD_SIZE-1));
     word[word_len] = '\0';
     if (isdigit(word[0])
         || ((word[0] == '-' || word[0] == '+')
@@ -33,16 +42,28 @@ int inter_word(const char *line, size_t word_start, size_t word_end) {
         stack_push(n);
     }
     else if (word_start == word_end - 1) {
-        double e;
-        switch (word[0]) {
-        case '+': e = stack_pop(); stack[stack_ptr-1] += e; break;
-        case '-': e = stack_pop(); stack[stack_ptr-1] -= e; break;
-        case '*': e = stack_pop(); stack[stack_ptr-1] *= e; break;
-        case '/': e = stack_pop(); stack[stack_ptr-1] /= e; break;
+        if (isalpha(word[0])) {
+            if (state == STORE) {
+                dict_set(word[0], stack_pop());
+                state = FETCH;
+            }
+            else {
+                stack_push(dict_get(word[0]));
+            }
+        }
+        else {
+            double e;
+            switch (word[0]) {
+            case '+': e = stack_pop(); stack[stack_ptr-1] += e; break;
+            case '-': e = stack_pop(); stack[stack_ptr-1] -= e; break;
+            case '*': e = stack_pop(); stack[stack_ptr-1] *= e; break;
+            case '/': e = stack_pop(); stack[stack_ptr-1] /= e; break;
+            }
         }
     }
     else {
         if      (!strcmp(word, "clear")) { stack_clear(); }
+        else if (!strcmp(word, "store")) { state = STORE; }
         else if (!strcmp(word, "sqrt"))  { stack_push(sqrt(stack_pop())); }
         else if (!strcmp(word, "ln"))    { stack_push(log(stack_pop())); }
         else if (!strcmp(word, "floor")) { stack_push(floor(stack_pop())); }
@@ -93,9 +114,9 @@ void print_stack(void) {
 }
 
 int main(void) {
-    char line[LINE_LIM];
+    char line[LINE_SIZE];
     print_stack();
-    while (fgets(line, LINE_LIM, stdin)) {
+    while (fgets(line, LINE_SIZE, stdin)) {
         inter_line(line, strlen(line));
         print_stack();
     }
